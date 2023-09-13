e8f027838c37d8a4c136776128d17a0fb39544cd
Tova-Rozovsky <tovaballons@gmail.com>
2023-09-13 12:16:48 +0300

    db creation

--- project/db/init.sql
+++ project/db/init.sql
@@ -1,13 +1,24 @@
 CREATE DATABASE chat-app-db;
 use chat-app-db;
 
-CREATE TABLE favorite_colors (
-  name VARCHAR(20),
-  color VARCHAR(10)
+CREATE TABLE rooms (
+  ID int NOT NULL,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (ID)
 );
 
-INSERT INTO favorite_colors
-  (name, color)
-VALUES
-  ('Lancelot', 'blue'),
-  ('Galahad', 'yellow');
+CREATE TABLE users (
+  ID int NOT NULL,
+  name varchar(255) NOT NULL,
+  password varchar NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE msg (
+  ID int NOT NULL,
+  msg varchar(255) NOT NULL,
+  room int FOREIGN KEY REFERENCES rooms(ID)
+  user int FOREIGN KEY REFERENCES users(ID)
+  PRIMARY KEY (ID)
+);
+
